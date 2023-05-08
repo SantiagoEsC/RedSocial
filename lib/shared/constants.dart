f9b02d8376137bd91636cdf56f22406252a2d437
Santiago Escobar <santiago.escobarc@uniagustiniana.edu.co>
2023-05-08 16:03:52 -0500

    Primer comit

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:188662397737:web:58aa267d658fb758a7d538";
+  static String apiKey = "AIzaSyCWBuLeP_k0NxaXn7ZR8ip2Re7LVKHCHCc";
+  static String messagingSenderId = "188662397737";
+  static String projectId = "chatapp-c6557";
+  final primaryColor = const Color(0xFFee7b64);
+}
